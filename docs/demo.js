ee5770e029bca6f44ec1418b792b48d925b10190
ben <beneaves01@hotmail.com>
2019-05-15 19:40:05 +0800

    init

--- docs/demo.js
+++ docs/demo.js
@@ -1,6 +1,6 @@
 
 //demo
-$('body').append('<div class="container-fluid"><h1 class="text-center mt-4 mb-4">ECDSA</h1><div class="row main"></div></div>')
+$('body').append('<div class="container-fluid"><h1 class="text-center mt-4 mb-4">jquery-ecdsa</h1><div class="row main"></div></div>')
 
 $.each(['curve','hash'], function(i,e){
   $('.main').append('<div class="col-sm-6 text-center mb-4"><label>select '+ e +'</label><select id="'+ e +'Key" class="custom-select"></select></div>')
@@ -25,8 +25,8 @@ $.each(['data','sign','verify'], function(i,e){
 })
 
 
-$.each(['test'], function(i,e){
-  $('.main').append('<div class="col-sm-6 text-center"><label>'+ e +' key</label><textarea id="'+ e +'Key" class="form-control keyOut"></textarea></div>')
+$.each(['test', 'result'], function(i,e){
+  $('.main').append('<div class="col-sm-6 text-center mb-4"><label>'+ e +'</label><textarea id="'+ e +'" class="form-control keyOut" readonly></textarea></div>')
 })
 
 $('#hashKey').off().on('change', function(){
@@ -34,10 +34,22 @@ $('#hashKey').off().on('change', function(){
 });
 
 $(document).ready(function() {
+
+  const ss = {
+    get: function(){
+      return JSON.parse(sessionStorage.getItem('time'));
+    },
+    set: function(i){
+      sessionStorage.setItem('time', JSON.stringify(i));
+      return
+    }
+  }
+
   $.each(['public','private','sign','verify'], function(i,e){
     $('#'+ e +'Key').attr('readonly','true')
   })
-  $('#signKey,#verifyKey').attr('readonly','true')
+
+  $('#signKey,#verifyKey').attr('readonly','true');
   $('#curveKey').on('change', function(){
     let crv = $(this).val()
 
@@ -49,12 +61,12 @@ $(document).ready(function() {
       //console.log(gen)
       $('#dataKey').off().on('keyup', function(){
 
-        let str = $(this).val(),
+        let str = $(this),
         hashVal = $('#hashKey').val();
-        $.ecSign(gen.private, str, hashVal, 'hex', function(err, res){
+        str.ecSign(gen.private, hashVal, 'hex', function(err, res){
           if(err){return console.log(err)}
           $('#signKey').val(res);
-          $.ecVerify(gen.public, res, str, hashVal, 'hex', function(err, res){
+          str.ecVerify(gen.public, res, hashVal, 'hex', function(err, res){
             if(err){return console.log(err)}
             if(res){
               $('#verifyKey').val('ecdsa test pass');
@@ -70,20 +82,6 @@ $(document).ready(function() {
 
   $('#curveKey').change();
 
-
-
-
-
-  const ss = {
-    get: function(){
-      return JSON.parse(sessionStorage.getItem('time'));
-    },
-    set: function(i){
-      sessionStorage.setItem('time', JSON.stringify(i));
-      return
-    }
-  }
-
   function shuffle(a) {
     for (let i = a.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -92,44 +90,35 @@ $(document).ready(function() {
     return a;
   }
 
-
-
   function test(crv,hash,digest, msg){
     let time = Date.now();
 
     $.ecGen(crv, function(err, gen){
-      $('#sig').ecSign(gen.private, hash, digest, function(err, res1){
-        if(err){return console.log(err)}
-        $('#sigRes').append(res1 + '\n')
-      })
-    })
-
-    $.ecGen(crv, function(err, gen){
       if(err){return console.log(err)}
-
       //console.log(gen)
       $.ecSign(gen.private, 'test', hash, digest, function(err, res){
         if(err){return console.log(err)}
         //console.log(res)
-        $.ecVerify(gen.public, res, 'test', hash, digest, function(err, res){
+        $.ecVerify(gen.public, res, 'test', hash, digest, function(err, res1){
           if(err){return console.log(err)}
-          if(res){
-            document.body.innerHTML += 'ecdsa '+ msg +' pass <br>';
+          if(res1){
+            $('#test').append('ecdsa '+ msg +' pass \n\n');
+            $('#result').append(res + '\n\n');
             console.log('ecdsa '+ msg +' pass')
             ss.set(ss.get()+1)
             if(ss.get() === 15){
-              document.body.innerHTML += 'Test finished in '+ (Date.now() - time) +'ms';
+              $('#test').append('Test finished in '+ (Date.now() - time) +'ms \n\n');
             }
             return;
           }
           console.log('ecdsa '+ msg +' fail')
-          document.body.innerHTML += 'ecdsa '+ msg +' fail <br>'
+          $('#test').append('ecdsa '+ msg +' fail \n\n');
+          $('#result').append(res + '\n');
           return ss.set(ss.get()+1)
         })
       })
     })
   }
-
 
   ss.set(0)
   let CRV = shuffle(['256','384','521']),
@@ -144,7 +133,6 @@ $(document).ready(function() {
         test(CRV[i], HASH[i], DIGEST[i], msg + ' test ' + (x + 1) +' round ' + (i + 1))
       }
     }
-
   } catch (err) {
     if(err){
       console.log(err)
